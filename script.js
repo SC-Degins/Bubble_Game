fca216bf97eba3f8ca4b1852e8859aedd3fcde3c
Suvam Chakraborti <nfgaming4523@gmail.com>
2024-02-08 00:09:02 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function Makebubble(){
     var a ="";
-    for(var i=1; i<=152; i++){
+    for(var i=1; i<=156; i++){
         var num = Math.floor(Math.random()*10);
         a += `<div class="bubble">${num}</div>`;    
     }
