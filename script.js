2aab40fc9b729579645a45a5cd895e21f8c6d791
Suvam Chakraborti <nfgaming4523@gmail.com>
2024-03-21 15:00:04 +0530

    scripting

--- script.js
+++ script.js
@@ -15,21 +15,22 @@ function getNewHit(){
     hitrn = Math.floor(Math.random() * 20);
     document.querySelector(".number").innerHTML = hitrn;
 }
-var timer = 120;
+var timer = 15;
 function Timeout(color){
     document.querySelector(".timer").style.color = color;
     var timerint = setInterval(function(){
-        if(timer >0){
-            timer--;
-            document.querySelector(".timer").textContent = timer;
-            // if(timer < 10){
-            //     Timeout("red");
-            //     timer += 1;
-            // }
-        }
-        else{
-            clearInterval(timerint);
-            document.querySelector("#panel-bottom").innerHTML = `<h1 class="over">Game Over</h1>`;
+        if (timer > 10) {
+          timer--;
+          document.querySelector(".timer").textContent = timer;
+        } else if (timer < 9) {
+          Timeout("red");
+          timer--;
+          document.querySelector(".timer").textContent = timer;
+        } else {
+          clearInterval(timerint);
+          document.querySelector(
+            "#panel-bottom"
+          ).innerHTML = `<h1 class="over">Game Over</h1>`;
         }
     },1000)
 }
@@ -65,44 +66,8 @@ function loaderup(){
 btn.addEventListener("click", function(){
     loader.style.top = "-110%";
     Timeout();
-    // changeColor();
 })
 }
-
-
-
-//   function changeColor(color) {
-//     document.querySelector(".timer").style.color = color;
-//   }
-
-//   // Function to start the countdown timer
-//   function startTimer(duration, display) {
-//     let timer = duration;
-//     let intervalId = setInterval(function () {
-//       timer--;
-//       display.textContent = timer;
-
-//       // Check if timer reaches 10 seconds
-//       if (timer <= 10) {
-//         // Change color
-//         changeColor("red");
-//       }
-
-//       // Check if timer reaches 0 seconds
-//       if (timer <= 0) {
-//         clearInterval(intervalId);
-//         display.textContent = "Time's up!";
-//       }
-//     }, 1000);
-//   }
-
-//   loader().onclick = function () {
-//     const tenSeconds = 10;
-//     const display = document.getElementById("countdown");
-//     startTimer(tenSeconds, display);
-//   };
-
-
 loaderup();
 Makebubble();
 getNewHit();
