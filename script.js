980b81ef629dd4dc9c126a6612d9904d546d3b65
Suvam Chakraborti <nfgaming4523@gmail.com>
2024-03-24 00:10:50 +0530

    finally done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Makebubble(){
     var a ="";
     for(var i=1; i<=153; i++){
         var num = Math.floor(Math.random()*20);
-        a += `<div class="bubble">${num}</div>`;    
+        a += `<div class="bubble" id="hover">${num}</div>`;    
     }
     document.querySelector("#panel-bottom").innerHTML = a;
     document.querySelector("#panel-bottom").addEventListener("click",function(){
@@ -16,6 +16,7 @@ function getNewHit(){
     document.querySelector(".number").innerHTML = hitrn;
 }
 var timer = 30;
+var count = document.querySelector("#count");
 function Timeout(){
     var timerint = setInterval(function(){
         if (timer > 0) {
@@ -23,12 +24,13 @@ function Timeout(){
           document.querySelector(".timer").textContent = timer;
           if (timer <= 10){
             document.querySelector(".timer").style.color = "red";
+            count.play();
           }
         } else {
           clearInterval(timerint);
           document.querySelector(
             "#panel-bottom"
-          ).innerHTML = `<h1 class="over">Game Over</h1>`;
+          ).innerHTML = `<h1 class="over">Game Over <br> Your Final Score is = ${score}</h1>`;
         }
     },1000)
 }
@@ -41,8 +43,9 @@ document.querySelector("#panel-bottom").addEventListener("click", function(dets)
     var clickedNum = Number(dets.target.textContent);
     if (clickedNum === hitrn) {
     scoreincrese();
-    Makebubble();
     getNewHit();
+    Makebubble(color_plate);
+    
 }
 })
 var mymusic = document.querySelector("#mymusic");
@@ -70,3 +73,47 @@ btn.addEventListener("click", function(){
 loaderup();
 Makebubble();
 getNewHit();
+var flag = 0;
+var setting = document.querySelector("#setting");
+setting.addEventListener("click", function(){
+    if(flag === 0){
+        document.querySelector("#setting").style.rotate = "175deg";
+        document.querySelector("#setiing-div").style.scale = "1";
+        document.querySelector(".overlay").style.display = "block";
+        flag = 1;
+    } else {
+        document.querySelector("#setiing-div").style.scale = "0";
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelector("#setting").style.rotate = "0deg";
+        flag = 0;
+    }
+});
+document.querySelector(".overlay").addEventListener("click", function(){
+    document.querySelector("#setiing-div").style.scale = "0";
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector("#setting").style.rotate = "0deg";
+    flag = 0;
+})
+var colors = document.querySelectorAll(".color");
+document.getElementById("color").style.border = "3px solid #000";
+document.querySelectorAll(".bubble").forEach(function(el){
+    el.style.background = "linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)";
+});
+function color(){
+colors.forEach(function(e){
+    e.addEventListener("click", function(el){
+        border();
+        var color = el.target.textContent;
+        el.target.style.border = "3px solid #000";
+        document.querySelectorAll(".bubble").forEach(function(elem){
+           var color_plate = elem.style.background = color;
+        })
+    })
+});
+};
+color();
+function border(){
+    document.querySelectorAll(".color").forEach(function(elem){
+        elem.style.border = "none";
+    })
+}
